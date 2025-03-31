afacf99b051c26a7739311f4f27a3e1e48d3e594
Rolando1233 <rolandoantonioflorespalafox@gmail.com>
2025-03-31 00:56:42 -0600

    Add properties from nextjs: Create modal, API endpoint, send data

--- djangobnb_backend/property/api.py
+++ djangobnb_backend/property/api.py
@@ -1,6 +1,8 @@
 from django.http import JsonResponse
 
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
+
+from .forms import PropertyForm
 from .models import Property
 from .serializers import PropertiesListSerializer
 
@@ -14,3 +16,17 @@ def properties_list(request):
     return JsonResponse({
         'data': serializer.data
     })
+
+@api_view(['POST', 'FILES'])
+def create_property(request):
+    form = PropertyForm(request.POST, request.FILES)
+
+    if form.is_valid():
+        property = form.save(commit=False)
+        property.landlord = request.user
+        property.save()
+
+        return JsonResponse({'succes': True})
+    else:
+        print('error', form.erros, form.non_field_errors)
+        return JsonResponse({'errors': form.errors.as_json()}, status=400)
