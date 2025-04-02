b057004942ae37b6557ce09c74c922d4d96ce8bb
Rolando1233 <rolandoantonioflorespalafox@gmail.com>
2025-04-02 00:17:30 -0600

    Book property

--- djangobnb_backend/property/api.py
+++ djangobnb_backend/property/api.py
@@ -3,8 +3,8 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 
 from .forms import PropertyForm
-from .models import Property
-from .serializers import PropertiesListSerializer, PropertiesDetailSerializer
+from .models import Property, Reservation
+from .serializers import PropertiesListSerializer, PropertiesDetailSerializer, ReservationsListSerializer
 
 @api_view(['GET'])
 @authentication_classes([])  # Aquí puedes agregar autenticación si es necesario
@@ -28,6 +28,18 @@ def properties_detail(request, pk):
 
     return JsonResponse(serializer.data)
 
+@api_view(['GET'])
+@authentication_classes([])
+@permission_classes([])
+def property_reservations(request, pk):
+    property = Property.objects.get(pk=pk)
+    reservations = property.reservations.all()
+
+    serializer = ReservationsListSerializer(reservations, many=True)
+
+    return JsonResponse(serializer.data, safe=False)
+    
+
 @api_view(['POST', 'FILES'])
 def create_property(request):
     form = PropertyForm(request.POST, request.FILES)
@@ -41,3 +53,32 @@ def create_property(request):
     else:
         print('error', form.erros, form.non_field_errors)
         return JsonResponse({'errors': form.errors.as_json()}, status=400)
+
+@api_view(['POST'])
+def book_prorperty(request, pk):
+    try:
+        start_date =request.POST.get('start_date', '')
+        end_date =request.POST.get('end_date', '')
+        number_of_nights =request.POST.get('number_of_nights', '')
+        total_price =request.POST.get('total_price', '')
+        guests =request.POST.get('guests', '')
+
+
+        property = Property.objects.get(pk=pk)
+
+        Reservation.objects.create(
+            property=property,
+            start_date=start_date,
+            end_date=end_date,
+            number_of_night=number_of_nights,
+            total_price=total_price,
+            guests=guests,
+            created_by=request.user
+        )
+
+        return JsonResponse({'success': True})
+    
+    except Exception as e:
+        print('Error', e)
+
+        return JsonResponse({'success': False})
--- djangobnb_backend/property/urls.py
+++ djangobnb_backend/property/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path( '',api.properties_list, name='properties_list'),
     path('create/', api.create_property, name='api_create_property'),
     path('<uuid:pk>/', api.properties_detail, name='api_properties_detail'),
+    path('<uuid:pk>/book/', api.book_prorperty, name='api_book_prorperty'),
+    path('<uuid:pk>/reservations/', api.property_reservations, name='api_property_reservations'),
 ]
